bb11c83bb88c9334ed17ce45fb102b499e76a74b
Asen Karadzhov <asen__7@abv.bg>
2024-04-25 22:22:48 +0300

    Added exercises

---
+++ Upr1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Upr1.Model;
+using Upr1.Others;
+using Upr1.View;
+using Upr1.ViewModel;
+
+namespace Upr1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            User user = new User()
+            {
+                Name = "Asen",
+                FakNum = "121212121",
+                Mail = "@asa.asd",
+                Password = "123",
+                Role = UserRolesEnum.STUDENT
+            };
+
+            UserViewModel uvm = new UserViewModel(user);
+            UserView uv = new UserView(uvm);
+
+            uv.Display();
+            uv.DisplayColor();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Upr2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Upr1.Model;
+using Upr1.Others;
+using Upr1.View;
+using Upr1.ViewModel;
+using Upr2.Others;
+
+namespace Upr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var user = new User
+                {
+                    Name = "Asen",
+                    Password = "password",
+                    Role = UserRolesEnum.STUDENT
+                };
+
+                var viewModel = new UserViewModel(user);
+                var view = new UserView(viewModel);
+
+                view.Display();
+
+                view.DisplayError();
+            }
+            catch (Exception ex)
+            {
+                var log = new ActionOnError(Delegates.Log);
+                var logF = new ActionOnError(Delegates.LogFile);
+                log(ex.Message);
+                logF(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executed in any case");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
